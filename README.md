274fe872d77fbe419debdbdd3dffe4de572d33b6
vohoang2005 <vohoang2005@gmail.com>
2022-05-22 19:34:22 -0700

    finish

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# DiceApp
+# Fun Dice App
+
+## Created a Fun Dice Rolling game. If you or a friend are undecisive, you can play this game and let the Winner choose!
+~~~
+Created by using:
+* HTML
+* CSS
+* Javascript
+
+Usage/Tools:
+* Download VS Code
+* Clone This Repo
+* Open a browser (Chrome/Edge) and click Roll The Dice to play :) 
+~~~
+## Below is a Video on how it works! Very Simple and Fun!
+
+
+### Created by: Chuong Vo
---
+++ index.js
@@ -0,0 +1,28 @@
+function rolldice() {
+
+    // Create randomnumber for dice 1.
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png to dice6.png
+var randomImageSource = "images/" + randomDiceImage; //image/dice1.png to dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource);
+
+// Create randomnumber for dice 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"; //dice1.png to dice6.png
+var randomImageSource2 = "images/" + randomDiceImage2; //image/dice1.png to dice6.png
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+    if(randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = ("Player 1 Won!");
+    }
+    else if(randomNumber2 > randomNumber1) {
+
+        document.querySelector("h1").innerHTML = ("Player 2 Won!");
+    }
+    else{
+        document.querySelector("h1").innerHTML = ("It's a DRAW!")
+    }
+}
